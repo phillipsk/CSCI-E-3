420db3c43847363b389d8afec9d69af72c788281
phillipsk <inquiries@kevinphillips.email>
2016-02-17 08:57:40 -0500

    up to Week 4 complete

---
+++ Week 3/assignment1.js
@@ -0,0 +1,25 @@
+/**
+ * Created by tech_ministry on 2/13/16.
+ */
+// Insert your code for #1 here
+var num_1 = 44;
+var num_2 = 55;
+var answer = num_1+num_2;
+console.log("The sum of "+num_1+" plus "+num_2+" is "+answer);
+
+
+// Insert your code for #2 here
+var user_num = prompt("What is your favorite number?")
+console.log(user_num);
+user_num++;
+console.log(user_num);
+
+// Insert your code for #3 here
+Math.ceil(Math.random()*11);
+
+
+// Insert your code for #4 here
+
+
+
+
---
+++ Week 4/Assignment 3/assignment3.js
@@ -0,0 +1,25 @@
+"use strict";
+// Insert your code for #1 here
+// initialized a new array with an index of 0-3 & debugging with a console log statement
+var Gospels = ['Matthew', 'Mark', 'Luke', 'John'];
+console.log(Gospels);
+// adding an additional string element to the beginning of the array
+Gospels.splice(0, 0, 'Paul');
+// Insert your code for #2 here
+// verifying splice method intentions with a console.log debug
+console.log(Gospels);
+// Insert your code for #3 here
+// iterating over each array element
+for (var i = 0; i < Gospels.length; i++) {
+    console.log(Gospels[i]);
+}
+var num = 0;
+// Insert your code for #4 here
+// Printing only integers which are odd up to 100
+// made use of the negation operator and only one increment statement
+for (var i = 0; i < 101; i++) {
+    if (num % 2 != 0) {
+        console.log(num);
+    }
+    num++;
+}
