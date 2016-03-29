bca52abc0ce1b2818f285ab6224659314d23bcb1
phillipsk <kjphillips@suffolk.edu>
2016-03-28 21:25:25 -0400

    Week 8

---
+++ Week 8/Assignment 2/Assignment2.js
@@ -0,0 +1,13 @@
+ // create a 'DIV' element node and assign it to a variable
+ var list = document.createElement("div");
+
+ // using setAttribute(), give it an ID attribute of "myNewDiv"
+list.setAttribute("myNewDiv","myNewDiv");
+
+ // create a text node with some text of your choosing, and assign it to a variable
+
+
+ // append you text node to your element
+
+
+ // append your new element to the existing div which has id="addNewOneHere"
