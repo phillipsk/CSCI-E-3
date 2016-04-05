beb2631dfa91237d8c57be834e1c941ada66d881
phillipsk <inquiries@kevinphillips.email>
2016-04-05 10:14:44 -0400

    finished

--- Week 9/Assignment 2/assignment2.js
+++ Week 9/Assignment 2/assignment2.js
@@ -7,23 +7,12 @@ window.onload=function(){
     highest_dom_el.addEventListener("click",function(evt){
        if (evt.target.id == "happyBtn"){
            var el = evt.target.id;
-           alert("Target id == " + evt.target.id + " and currentTarget == "+ evt.currentTarget.id);
-           //logMessage(evt.target.id.innerHTML);
-           //logMessage(el.innerHTML);
            logMessage(happy.innerHTML);
        }
         else {
-           //evt == gloomy;
-           //logMessage(gloomy.innerHTML);
+           logMessage(gloomy.innerHTML);
        }
     });
-
-/*    happy.addEventListener("click", function(evt){
-        logMessage(evt.currentTarget.innerHTML);
-    });
-    gloomy.addEventListener("click", function(evt){
-        logMessage(evt.currentTarget.innerHTML);
-    });*/
 };
 
 
