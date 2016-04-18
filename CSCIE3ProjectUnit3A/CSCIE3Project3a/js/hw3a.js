b4ffa450166d4292c34be5daedb0d23acb59fe0c
phillipsk <inquiries@kevinphillips.email>
2016-04-18 02:27:43 -0400

    completed partA

--- CSCIE3ProjectUnit3A/CSCIE3Project3a/js/hw3a.js
+++ CSCIE3ProjectUnit3A/CSCIE3Project3a/js/hw3a.js
@@ -10,56 +10,37 @@
  b. Add the SPAN elements to the original DIV
  3. Add a handler to the SPAN elements, or to the DIV, which causes the style on the SPAN to change on mouseover.
  */
-window.onload = function() {
-
-    var transcript_primitive = document.getElementById("transcriptText").innerHTML;
-    var innerHTML_array = transcript_primitive.split(" ");
-
-    /* I learned of this for loop structure from
-     * http://stackoverflow.com/questions/5767325/remove-a-particular-element-from-an-array-in-javascript
-     * I am aware of other array methods of cleaning up an array's values such as filter, but I really
-     * enjoyed the decrement functionality of this for loop approach*/
-    for(var i = innerHTML_array.length - 1; i >= 0; i--) {
-        if(innerHTML_array[i] === "") {
-            innerHTML_array.splice(i, 1);
-        }
-    }
-
-    /* Clearing the contents on the div element prior to refilling via an itteration with new elements and attributes*/
-    //transcript_primitive = " ";
-    document.getElementById("transcriptText").innerHTML = "";
-
-    var arrayLength = innerHTML_array.length;
-    for (var i = 0; i < arrayLength; i++){
-        var el = document.createElement("span"); // new SPAN
-        el.setAttribute("class","word");
-        el.setAttribute("id","word"+i);
-
-        /* Space added to the end to logically print the words WITH spaces*/
-        var t = document.createTextNode(innerHTML_array[i]+" ");
-        el.appendChild(t);
-        //var t = document.createTextNode(innerHTML_array[i]);
-        //el.appendChild(t);
-
-        //transcript_primitive.appendChild(el);
-        var a = document.getElementById("transcriptText");
-        a.appendChild(el);
-
-        /*p.insertBefore(d, arrayLength[i]);   // insert it right before text node
-         d.appendChild(arrayLength[i]);       // move text node into the span
-         d.setAttribute("class", "searchHit");  // apply highlight*/
-    }
-
-    var elMouseover = document.getElementById("transcriptText");
-    elMouseover.addEventListener("mouseover",function () {
-        elMouseover.style.color = "yellow";
-    });
-    elMouseover.addEventListener("mouseout",function () {
-        elMouseover.style.color = "black";
-    });
-    
-    function custom_mouseOver() {
-        
-    }
-
+window.onload = function () {
+    var transformBtn = document.getElementById('divideTranscript');
+    transformBtn.addEventListener('click', transform, false);
 };
+
+function transform() {
+    var textDiv = document.getElementById("transcriptText");
+    var initText = textDiv.innerHTML;
+    var text = initText.split(' ');
+
+    var textLen = text.length;
+    document.getElementById("transcriptText").innerHTML = "";
+    for (var i = 0; i < textLen; i++) {
+        var word = text[i];
+
+        var wordSpan = document.createElement('span');
+        var spaceSpan = document.createElement('span');
+        var wordNode = document.createTextNode(word);
+        var spaceNode = document.createTextNode(' ');
+
+        wordSpan.addEventListener('mouseover', function () {
+            this.setAttribute('class', 'highlight')
+        });
+        wordSpan.addEventListener('mouseout', function (evt) {
+            this.setAttribute('class', 'default')
+        });
+
+        wordSpan.appendChild(wordNode);
+        spaceSpan.appendChild(spaceNode);
+        textDiv.appendChild(wordSpan);
+        textDiv.appendChild(spaceSpan);
+    }
+
+}
